49c48420d30d0812aef886518b43ae545d1f57d2
Carlos A <caralla@upv.es>
2022-06-29 18:32:22 +0200

    rename of project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# osquery
+# novaquery
 Utility to query OpenStack nova for servers, the state, flavors, etc. using json friendly queries
--- novaquery/__init__.py
+++ novaquery/__init__.py
@@ -59,18 +59,18 @@ def main():
     """
     This application enables to retrieve the servers from OpenStack NOVA, using its API. And it enables to filter the servers by
     their attributes. E.g. to query the servers with the attribute "status" set to "ACTIVE", it is possibile to use the following command:
-        osquery status=ACTIVE
+        novaquery status=ACTIVE
 
     This command also enables to group the results by one attribute, using the following command will obtain the servers that are not
     in the "ACTIVE" status, grouped by the attribute "status":
-        osquery status!=ACTIVE -g status
+        novaquery status!=ACTIVE -g status
 
     Examples:
         - command line to retrieve active VMs, grouped by host:
-            osquery status=ACTIVE -A -g OS-EXT-SRV-ATTR:host
+            novaquery status=ACTIVE -A -g OS-EXT-SRV-ATTR:host
 
         - command line to retrieve the names and id of VMs that have pci_passthrough devices, grouped by host name:
-            osquery flavor.extra_specs.pci_passthrough:alias -f name -f id -g OS-EXT-SRV-ATTR:host
+            novaquery flavor.extra_specs.pci_passthrough:alias -f name -f id -g OS-EXT-SRV-ATTR:host
 
     """
     parser = argparse.ArgumentParser(allow_abbrev=False, description=main.__doc__, formatter_class=argparse.RawTextHelpFormatter)
